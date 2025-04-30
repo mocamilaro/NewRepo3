9e9c398a03f040328ae0b1c354b97422d2d2c2ee
SebasCiro <134970820+SebasCiro@users.noreply.github.com>
2025-04-30 09:54:46 -0500

    Create Registro.cshtml.cs
    Behind del registro

---
+++ asp_presentacion/Pages/Registro.cshtml.cs
@@ -0,0 +1,160 @@
+using lib_entidades.Modelos;
+using lib_presentaciones.Interfaces;
+using lib_utilidades;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace asp_presentacion.Pages
+{
+
+    public class RegistroModel : PageModel
+    {
+        private IPacientePresentacion? iPresentacion = null;
+
+        public RegistroModel(IPacientePresentacion iPresentacion)
+        {
+            try
+            {
+                this.iPresentacion = iPresentacion;
+                Filtro = new Paciente();
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+        [BindProperty] public Enumerables.Ventanas Accion { get; set; }
+        [BindProperty] public Paciente? Actual { get; set; }
+        [BindProperty] public Paciente? Filtro { get; set; }
+        [BindProperty] public List<Paciente>? Lista { get; set; }
+
+        public virtual void OnGet() { OnPostBtRefrescar(); }
+
+        public void OnPostBtRefrescar()
+        {
+            try
+            {
+                var variable_session = HttpContext.Session.GetString("Usuario");
+                if (String.IsNullOrEmpty(variable_session))
+                {
+                    HttpContext.Response.Redirect("/");
+                    return;
+                }
+                Filtro!.Nombre = Filtro!.Nombre ?? "";
+
+                Accion = Enumerables.Ventanas.Listas;
+                var task = this.iPresentacion!.Buscar(Filtro!, "NOMBRE");
+                task.Wait();
+                Lista = task.Result;
+                Actual = null;
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+        public virtual void OnPostBtNuevo()
+        {
+            try
+            {
+                Accion = Enumerables.Ventanas.Editar;
+                Actual = new Paciente();
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+        public virtual void OnPostBtModificar(string data)
+        {
+            try
+            {
+                OnPostBtRefrescar();
+                Accion = Enumerables.Ventanas.Editar;
+                Actual = Lista!.FirstOrDefault(x => x.Id.ToString() == data);
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+        public virtual void OnPostBtGuardar()
+        {
+            try
+            {
+                Accion = Enumerables.Ventanas.Editar;
+                Task<Paciente>? task = null;
+                if (Actual!.Id == 0)
+                    task = this.iPresentacion!.GuardarPaciente(Actual!);
+                else
+                    task = this.iPresentacion!.Modificar(Actual!);
+                task.Wait();
+                Actual = task.Result;
+                Accion = Enumerables.Ventanas.Listas;
+                OnPostBtRefrescar();
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+        public virtual void OnPostBtBorrarVal(string data)
+        {
+            try
+            {
+                OnPostBtRefrescar();
+                Accion = Enumerables.Ventanas.Borrar;
+                Actual = Lista!.FirstOrDefault(x => x.Id.ToString() == data);
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+        public virtual void OnPostBtBorrar()
+        {
+            try
+            {
+                var task = this.iPresentacion!.Borrar(Actual!);
+                Actual = task.Result;
+                OnPostBtRefrescar();
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+        public void OnPostBtCancelar()
+        {
+            try
+            {
+                Accion = Enumerables.Ventanas.Listas;
+                OnPostBtRefrescar();
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+        public void OnPostBtCerrar()
+        {
+            try
+            {
+                if (Accion == Enumerables.Ventanas.Listas)
+                    OnPostBtRefrescar();
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+    }
+}
