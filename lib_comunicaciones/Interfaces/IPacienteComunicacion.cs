71a45fea5e41eb689482b73df08b3309bc075544
SebasCiro <134970820+SebasCiro@users.noreply.github.com>
2025-04-30 10:08:01 -0500

    Update IPacienteComunicacion.cs
    guardarPaciente en interfaces en capa de comunicaciones

--- lib_comunicaciones/Interfaces/IPacienteComunicacion.cs
+++ lib_comunicaciones/Interfaces/IPacienteComunicacion.cs
@@ -5,6 +5,7 @@
         Task<Dictionary<string, object>> Listar(Dictionary<string, object> datos);
         Task<Dictionary<string, object>> Buscar(Dictionary<string, object> datos);
         Task<Dictionary<string, object>> Guardar(Dictionary<string, object> datos);
+        Task<Dictionary<string, object>> GuardarPaciente(Dictionary<string, object> datos);
         Task<Dictionary<string, object>> Modificar(Dictionary<string, object> datos);
         Task<Dictionary<string, object>> Borrar(Dictionary<string, object> datos);
     }
